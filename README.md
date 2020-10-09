ada659ab78450c4d574947cd1919f6ae12eba73f
Vinaykumar Dubey <vinay.dubey0990@gmail.com>
2020-10-09 12:44:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hotel-Reservation-Management
+ Hotel-Reservation-System with spring boot and later adding Angular as frontend
---
+++ src/main/java/com/vinayak/HotelReservationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vinayak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationManagementApplication.class, args);
+	}
+
+}
