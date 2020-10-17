1e02c5e1e6c140ddc3b18dfb3cc3175f3e35be66
Vinaykumar Dubey <vinay.dubey0990@gmail.com>
2020-10-17 23:22:28 +0530

    final commit for now
    end for now

--- src/main/java/com/vinayak/web/controllers/GuestController.java
+++ src/main/java/com/vinayak/web/controllers/GuestController.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.vinayak.business.service.GuestService;
@@ -19,11 +20,12 @@ public class GuestController {
 	@Autowired
 	private GuestService guestService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String getReservations(@RequestParam(value = "id", required = false) Long id, Model model) {
+	@GetMapping
+	public String getGuests(@RequestParam(value = "id", required = false) Long id, Model model) {
 		List<Guest> guestList = this.guestService.getAllGuest();
 		model.addAttribute("guests", guestList);
 		return "guests";
 	}
+
 
 }
